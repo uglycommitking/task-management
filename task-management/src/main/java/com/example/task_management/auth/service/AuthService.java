9f1832f3d10757b36ffab2e7bc29afaf9717687b
leandoero <seventeen.iinc@gmail.com>
2026-07-25 21:37:41 +0300

    add hash for passwords

--- task-management/src/main/java/com/example/task_management/auth/service/AuthService.java
+++ task-management/src/main/java/com/example/task_management/auth/service/AuthService.java
@@ -4,6 +4,7 @@ import com.example.task_management.auth.mapper.AuthMapper;
 import com.example.task_management.auth.model.AuthRequest;
 import com.example.task_management.auth.model.AuthResponse;
 import com.example.task_management.auth.repository.AuthRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,22 +12,23 @@ public class AuthService {
 
     private final AuthRepository authRepository;
     private final AuthMapper mapper;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthService(AuthRepository authRepository, AuthMapper mapper){
+    public AuthService(AuthRepository authRepository, AuthMapper mapper, PasswordEncoder passwordEncoder){
         this.authRepository = authRepository;
         this.mapper = mapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public AuthResponse createUser(AuthRequest request){
         if(authRepository.existsByUsername(request.username())){
            throw new IllegalArgumentException("Имя пользователя " + request.username() + " занято");
         }
+        String hashed = passwordEncoder.encode(request.password());
         var authEntity = mapper.toEntity(request);
-        var response = mapper.toDomain(authRepository.save(authEntity));
-        return response;
+        authEntity.setPassword(hashed);
+        return mapper.toDomain(authRepository.save(authEntity));
     }
-
-
 
 
 }
